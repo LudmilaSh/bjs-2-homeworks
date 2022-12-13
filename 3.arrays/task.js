9adb6aa34f10013b81528715923230c4f33288cc
Людмила Трибушинина <shilova_lu@mail.ru>
2022-12-13 20:48:13 +0400

    Third homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
+  return isEquals = arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, array) => acc + item /array.length, 0);
+   
 }
